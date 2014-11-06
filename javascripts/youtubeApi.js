e8f2604e6a81b2fed23c96f6e9a272438e00da48
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-06 15:56:28 +0530

    still testing

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -2,9 +2,11 @@
 // https://developers.google.com/youtube/terms
 
 // Helper function to display JavaScript value on HTML page.
+var youtubeResponse = "";
 function showResponse(response) {
     var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('response').innerHTML += responseString;
+	youtubeResponse = responseString;
+    //document.getElementById('response').innerHTML += responseString;
 }
 
 function returnResponse(response) {
