4ebcb41e41e0b3e45871c1b9a1a765a3a71a8211
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-06 12:26:08 +0530

    moved youtubeapi functions to main js

--- javascripts/myJSFunctions.js
+++ javascripts/myJSFunctions.js
@@ -9,6 +9,65 @@ var errIdCnt = 0;
 
 divArr = [];
 
+
+
+
+
+
+
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See http://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyBGyrLRM1SQEyCvPOsYAosM0xO4zGWLkX0');
+
+    search();
+}
+
+// After the API loads, call a function to enable the search box.
+function handleAPILoaded() {
+	$('#search-button').attr('disabled', false);
+}
+
+function search(queryTxt) {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'id',
+		q: queryTxt
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
+}
+
+
+
+
+
+
+
+
+
 function hideErrorDiv(errorId)
 {
 	var displayState = document.getElementById(errorId).style.display;
@@ -276,6 +335,7 @@ function youtubeSearch()
 {
 	var sbInpObj = document.getElementById('searchBoxInp');
 	var searchTxt = sbInpObj.value;
+	search(searchTxt);
 	if(searchTxt)
 	{
 		$.getScript("youtubeApi.js", function(){
