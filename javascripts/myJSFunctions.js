833ed2bf863273523f0041c4c5a12857b33e642a
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-06 16:10:00 +0530

    introduced timer

--- javascripts/myJSFunctions.js
+++ javascripts/myJSFunctions.js
@@ -357,6 +357,7 @@ function youtubeSearch()
 	{
 		$.getScript("http://praneetsharma.github.io/javascripts/youtubeApi.js", function(){
 			searchResultsResponse = search(searchTxt);
+			setTimeout(function(){},500);
 			searchResultsResponse = document.getElementById('response').innerHTML;
 			parseSearchResponse(youtubeResponse);
 		});
