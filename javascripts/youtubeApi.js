1202e310aeae2af2a911b98891459c2efc853fdb
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-07 10:56:33 +0530

    moved some functions to youtubeapi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -3,6 +3,74 @@
 
 // Helper function to display JavaScript value on HTML page.
 var youtubeResponse = "";
+
+
+
+function parseSearchResponse(response)
+{
+	obj = JSON.parse(response);
+	var i;
+	
+	for(i=0;i<5;i++)
+	{
+		var title = obj.items[i].snippet.title;
+		var thumbnail = obj.items[i].snippet.thumbnails.medium;
+		var url = "https://www.youtube.com/watch?v=" + obj.times[i].id.videoId;
+		var videoDescr = obj.items[i].snippet.description;
+		var uploaderInfo = obj.items[i].snippet.channelTitle;
+		oneSearchResult(url, thumbnail, title, uploaderInfo, videoDescr);
+	}
+}
+
+
+
+
+function oneSearchResult(url, imgSrc, title, uploaderInfo, videoDescr)
+{
+	var parentObj = document.getElementById('singleSearchResult');
+	
+	var field = document.createElement('fieldset');
+	
+	//adding thumbnail
+	var thumbnailDiv = document.createElement('div');
+	thumbnailDiv.class="thumbnail";
+	var e1 = document.createElement('a');
+	e1.href = "javascript:addLoopDiv()";
+	var img = document.createElement("img");
+	img.src = imgSrc;
+	img.width = '185';
+	img.height = '104';
+	e1.appendChild(img);
+	thumbnailDiv.appendChild(e1);
+	field.appendChild(thumbnailDiv);
+	
+	//adding title, uploaderInfo, and videoDescr
+	var videoInfoDiv = document.createElement('div');
+	var h3 = document.createElement('h3');
+	h3.class = 'videoTitle';
+	var e2 = document.createElement('a');
+	e2.href = "javascript:addLoopDiv()";
+	e2.innerHTML = title;
+	h3.appendChild(e2);
+	field.appendChild(h3);
+	var metaDiv = document.createElement('div');
+	metaDiv.class = 'uploaderMeta';
+	metaDiv.innerHTML = uploaderInfo;
+	field.appendChild(metaDiv);
+	var descrDiv = document.createElement('div');
+	descrDiv.class = 'videoDescr';
+	descrDiv.innerHTML = videoDescr;
+	field.appendChild(descrDiv);
+	
+	
+	
+	
+	parentObj.appendChild(field);
+}
+
+
+
+
 function showResponse(response) {
     var responseString = JSON.stringify(response, '', 2);
 	//youtubeResponse = responseString;
@@ -45,6 +113,7 @@ function search(queryTxt) {
 	request.execute(function(response){
 		var str = JSON.stringify(response.result);
 		youtubeResponse = str;
+		parseSearchResponse(str);
 	});
 	
 }
