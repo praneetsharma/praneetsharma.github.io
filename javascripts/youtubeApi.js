ea4d387e8db79652dcdf4cb88b3c2b54141fda18
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-10 14:49:19 +0530

    testing youtube api

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -92,29 +92,25 @@ function oneSearchResult(url, imgSrc, title, uploaderInfo, videoDescr, i)
 
 
 
-/*function onYouTubeIframeAPIReady() {
-
+function onYouTubeIframeAPIReady() {
 
 	// Load the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/player_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	  var tag = document.createElement('script');
+	  tag.src = "https://www.youtube.com/player_api";
+	  var firstScriptTag = document.getElementsByTagName('script')[0];
+	  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-
-  //var player;
-  player = new YT.Player('player', {
-    width: 1280,
-    height: 720,
-    videoId: 'M7lc1UVf-VE',
-    events: {
-      'onReady': onPlayerReady,
-      'onPlaybackQualityChange': onPlayerPlaybackQualityChange,
-      'onStateChange': onPlayerStateChange,
-      'onError': onPlayerError
-    }
-  });
-}*/
+	  // Replace the 'ytplayer' element with an <iframe> and
+	  // YouTube player after the API code downloads.
+	  var player;
+	  function onYouTubePlayerAPIReady() {
+		player = new YT.Player('ytplayer', {
+		  height: '390',
+		  width: '640',
+		  videoId: 'M7lc1UVf-VE'
+		});
+	  }
+}
 
 function playVideoInBox()
 {
