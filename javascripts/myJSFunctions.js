d346a0b8c42fbad8484a07ca318fc16c3c2c5ec4
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-29 14:08:28 +0530

    Updated myJSFunctions.js

--- javascripts/myJSFunctions.js
+++ javascripts/myJSFunctions.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function() {
   	$("#leftDiv").hover(
       function () {
         $("#leftDiv").animate({
-            right: '40px'
+          'marginLeft' : "-=30px"
         });
       
        },
