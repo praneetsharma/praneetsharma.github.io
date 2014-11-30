ca6d896e3be9c930d09889fbc4aa31516ad90bb2
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-30 23:17:26 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -290,7 +290,8 @@ function parseUrlSearchResponse(response)
     var videoDescr = obj.items[i].snippet.description;
     var uploaderInfo = obj.items[i].snippet.channelTitle;
   	
-  	
+  	var localUrl = url;	
+  
   	var parentObj = document.getElementById('loopMain2Child');
 	
 	var field = document.createElement('fieldset');
