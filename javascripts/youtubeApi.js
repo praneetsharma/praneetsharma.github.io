938c7a41769aa71e7aa16b4bd3637778138800a7
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-30 19:09:30 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -501,7 +501,6 @@ function searchResponsePrevious(queryTxt) {
 
 function searchResponseNext(queryTxt) {
     // Use the JavaScript client library to create a search.list() API call.
-  	alert(query);
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
       	type: 'video',
