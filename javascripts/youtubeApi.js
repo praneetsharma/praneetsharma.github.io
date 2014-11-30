f62310725c68a7a3cf1044d28a78409ccebf1573
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-30 23:40:46 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -109,7 +109,7 @@ function fillSinglePopVid(url, imgSrc, title, uploaderInfo, videoDescr, i)
 	thumbnailDiv.id = 'tnail-'+i;
 	thumbnailDiv.style.display = 'inline-block';
 	var e1 = document.createElement('a');
-	e1.href = "javascript:addLoopDivSpcl('"+localUrl+"')";
+	//e1.href = "javascript:addLoopDivSpcl('"+localUrl+"')";
 	var img = document.createElement("img");
 	img.src = imgSrc;
 	img.width = '185';
