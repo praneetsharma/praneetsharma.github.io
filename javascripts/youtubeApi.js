8afe4a9f1d5da583a085c32b9855ae8f78d3385c
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-30 12:06:01 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -43,7 +43,7 @@ function fillPopVidBox(response)
       	$('#box').animate({'top':'50px'},200);
     });
     
-  	document.getElementById('boxChild').style.height = '70%';
+  	document.getElementById('box').style.height = '70%';
   
   
   	obj = JSON.parse(response);
