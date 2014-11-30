6484e079028b8896c03b0664091bc043c557589b
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-11-30 18:44:53 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -22,7 +22,7 @@ function setPlayer(player)
 
 function clearSearchBox()
 {
-  	pageNUm = 0;
+  	pageNum = 0;
 	var parentObj = document.getElementById('searchResultsList');
 	var removeObj = document.getElementById('singleSearchResult');
 	parentObj.removeChild(removeObj);
