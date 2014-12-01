4c7b48a4f3bbdc569eaa458c1a0543890822c578
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-12-01 17:46:19 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -326,6 +326,7 @@ function fillSingleSearchVidPop(url, imgSrc, title, uploaderInfo, videoDescr, i)
 function parseUrlSearchResponse(response)
 {
  
+  	
   	obj = JSON.parse(response);
   
   	var i =0;
