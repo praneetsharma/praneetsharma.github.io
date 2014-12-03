7eccedf31c8bd93dc290e4277573cafca804bfa0
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-12-03 19:23:48 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -789,8 +789,6 @@ function onPlayerStateChange(event)
   
   if(event.data == YT.PlayerState.ENDED)
   {
-    	//loadNextVid('DdTLXQNOA2s', 5, 60, 'large');
-    	//playVideosInPlaylist();
     	playNextVidInPlaylist();
   }
   
