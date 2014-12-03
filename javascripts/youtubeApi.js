8d0f0a5c0a804d63934c9cda7961ae96a1c54021
Praneet Sharma <praneet10061@iiitd.ac.in>
2014-12-03 19:24:33 +0530

    Updated youtubeApi.js

--- javascripts/youtubeApi.js
+++ javascripts/youtubeApi.js
@@ -815,7 +815,7 @@ function loadNextVid(vidId)
 {
     url = "https://www.youtube.com/v/"+vidId+"?version=3&hl=en&fs=1&enablejsapi=1";
    // player.loadVideoByUrl({'mediaContentUrl': url, 'startSeconds': startSec, 'endSeconds': endSec, 'suggestedQuality': vidQual});
-  	player.loadVideoByUrl({'mediaContentUrl': url, 'startSeconds': startSec, 'endSeconds': endSec});
+  	player.loadVideoByUrl({'mediaContentUrl': url});
     //player.loadVideoByUrl(url, 0);
     player.playVideo();
 }
